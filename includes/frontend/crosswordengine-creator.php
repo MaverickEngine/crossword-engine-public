e9ca3887d9cd7c6dc61ad97d88360c88c0f9f806
Jason Norwood-Young <jason@10layer.com>
2022-06-07 12:42:06 +0200

    CrosswordEngine Creator Shortcodes

---
+++ includes/frontend/crosswordengine-creator.php
@@ -0,0 +1,49 @@
+<?php
+class CrosswordEngineCreator {
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
+    public function generate($meta = false) {
+        if (get_option('crosswordengine_developer_mode')) {
+            wp_enqueue_style('crosswordengine-creator-style', plugin_dir_url( __FILE__ ) . '../../dist/crosswordengine.creator.css', [], "0.0.4");
+            wp_enqueue_script( "crosswordengine-creator-script", plugin_dir_url(__FILE__) . "../../dist/crosswordengine.creator.js", [], "0.0.4", true );
+        } else {
+            wp_enqueue_style('crosswordengine-creator-style', plugin_dir_url( __FILE__ ) . '../../dist/crosswordengine.creator.min.css', [], "0.0.4");
+            wp_enqueue_script( "crosswordengine-creator-script", plugin_dir_url(__FILE__) . "../../dist/crosswordengine.creator.min.js", [], "0.0.4", true );
+        }
+        $empty_xd = "
+
+        A1.  ~           
+        A11.  ~           
+        A12.  ~           
+        A13.  ~           
+        A14.  ~           
+        A15.  ~           
+        A16.  ~           
+        A17.  ~           
+        A18.  ~           
+        A19.  ~           
+        
+        D1.  ~           
+        D2.  ~           
+        D3.  ~           
+        D4.  ~           
+        D5.  ~           
+        D6.  ~           
+        D7.  ~           
+        D8.  ~           
+        D9.  ~           
+        D10.  ~           
+        ";
+        $script = 'var xd = ' . json_encode($empty_xd) . '; ';
+        wp_add_inline_script('crosswordengine-script', $script, 'before');
+        return '<div id="crosswordengine-creator-container"></div>';
+    }
+
+    public function render($meta = false) {
+        echo $this->generate($meta);
+    }
+
+}
+?>
--- includes/frontend/crosswordengine-shortcode.php
+++ includes/frontend/crosswordengine-shortcode.php
@@ -4,6 +4,7 @@ class CrosswordEngineShortcode {
         add_shortcode('crosswordengine', array($this, 'crosswordengine_shortcode'));
         add_shortcode('crossword-engine', array($this, 'crosswordengine_shortcode'));
         add_shortcode('crossword_engine', array($this, 'crosswordengine_shortcode'));
+        add_shortcode('crosswordengine-creator', array($this, 'crosswordengine_creator_shortcode'));
     }
 
     public function crosswordengine_shortcode($atts) {
@@ -14,6 +15,15 @@ class CrosswordEngineShortcode {
         require_once plugin_dir_path( dirname( __FILE__ ) ).'frontend/crosswordengine-view.php';
         $crossword_view = new CrosswordEngineView($atts['id']);
         return $crossword_view->generate($atts['meta']);
+    }
+
+    public function crosswordengine_creator_shortcode($atts) {
+        $atts = shortcode_atts(array(
+            'id' => '',
+        ), $atts);
+        require_once plugin_dir_path( dirname( __FILE__ ) ).'frontend/crosswordengine-creator.php';
+        $crossword_creator = new CrosswordEngineCreator($atts['id']);
+        return $crossword_creator->generate();
     }
 }
 ?>
