052c02f65f41674db8792a69d2e18d9764af105c
Jason Norwood-Young <jason@10layer.com>
2022-06-12 13:45:43 +0200

    Constants not being uploaded

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 git clone git@github.com:MaverickEngine/crossword-engine.git
 cp -a crossword-engine/includes .
 cp crossword-engine/crosswordengine.php .
-# cp crossword-engine/crosswordengine_constants.php .
+cp crossword-engine/crosswordengine_constants.php .
 cd crossword-engine
 yarn
 yarn run build:dev
